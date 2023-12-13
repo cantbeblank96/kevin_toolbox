cd8c429d0b13dcb2621f1330bd83305689a208b8
ubuntu_in_sensetime <xukaiming@tetras.ai>
2023-12-13 16:38:28 +0800

    modify patches.for_os.remove() to support remove link

--- kevin_toolbox/patches/for_os/remove.py
+++ kevin_toolbox/patches/for_os/remove.py
@@ -4,13 +4,15 @@ import shutil
 
 def remove(path, ignore_errors=False):
     """
-        移除文件/文件夹
+        移除文件/文件夹/软连接
 
         返回:
             boolean 是否成功
     """
     try:
-        if os.path.isfile(path):  # 移除文件
+        if os.path.islink(path):  # 移除软连接
+            os.unlink(path)
+        elif os.path.isfile(path):  # 移除文件
             os.remove(path)
         elif os.path.isdir(path):  # 移除文件夹
             shutil.rmtree(path=path, ignore_errors=False)
