f76b3068c11ab3319a1d53a37d86d5c433ee5233
cantbeblank96 <xukaiming1996@163.com>
2025-01-24 22:15:25 +0800

    add check_validity_and_uninstall/version_and_update() to env_info

--- kevin_toolbox/env_info/check_validity_and_uninstall.py
+++ kevin_toolbox/env_info/check_validity_and_uninstall.py
@@ -1,6 +1,4 @@
-import os
 import subprocess
-import argparse
 import time
 
 """
@@ -8,22 +6,44 @@ import time
 - 若超过指定的有效期，则卸载。
 """
 
-out_parser = argparse.ArgumentParser(description='check_validity_and_uninstall')
-out_parser.add_argument('--package_name', type=str, required=True)
-out_parser.add_argument('--expiration_timestamp', type=int, required=False, default=1e10)
-out_parser.add_argument('--verbose', type=int, required=False, default=1)
-args = out_parser.parse_args().__dict__
+DEFAULT_EXPIRATION_TIMESTAMP = 1e10
 
-cur_timestamp = time.time()
 
-if cur_timestamp > args["expiration_timestamp"]:
-    ex = subprocess.Popen(f'pip uninstall {args["package_name"]} --yes', shell=True, stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT)
-    out, _ = ex.communicate()
-    res = out.decode().strip()
-else:
-    res = "still within the validity period"
+def check_validity_and_uninstall(package_name, expiration_timestamp=DEFAULT_EXPIRATION_TIMESTAMP):
+    """
+        检查当前机器时间是否超过 expiration_timestamp 指定的有效期，若超过则卸载 package_name 对应的库
+    """
+    cur_timestamp = time.time()
 
-if args["verbose"]:
-    # print(args)
-    print(res)
+    b_success_uninstalled = False
+    if cur_timestamp > expiration_timestamp:
+        ex = subprocess.Popen(f'pip uninstall {package_name} --yes', shell=True, stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT)
+        out, _ = ex.communicate()
+        msg = out.decode().strip()
+        if ex.returncode == 0:
+            b_success_uninstalled = True
+    else:
+        msg = "still within the validity period"
+
+    res_s = dict(cur_timestamp=cur_timestamp, expiration_timestamp=expiration_timestamp,
+                 b_success_uninstalled=b_success_uninstalled, msg=msg)
+    return res_s
+
+
+if __name__ == '__main__':
+    import argparse
+
+    out_parser = argparse.ArgumentParser(description='check_validity_and_uninstall')
+    out_parser.add_argument('--package_name', type=str, required=True)
+    out_parser.add_argument('--expiration_timestamp', type=float, required=False, default=DEFAULT_EXPIRATION_TIMESTAMP)
+    out_parser.add_argument('--verbose', type=lambda x: bool(eval(x)), required=False, default=True)
+    args = out_parser.parse_args().__dict__
+
+    b_version = args.pop("verbose")
+
+    res_s_ = check_validity_and_uninstall(**args)
+
+    if b_version:
+        for k, v in res_s_.items():
+            print(f"{k}: {v}")
--- kevin_toolbox/env_info/check_version_and_update.py
+++ kevin_toolbox/env_info/check_version_and_update.py
@@ -1,51 +1,72 @@
-import os
 import subprocess
-import argparse
-import version
 
-"""
-检查当前版本
-- 若在可用版本中，有比当前版本更高的版本，则更新到可以获取到的最新版本。
-"""
+try:
+    from kevin_toolbox.env_info import version
+except:
+    import version
 
-out_parser = argparse.ArgumentParser(description='check_version_and_update')
-out_parser.add_argument('--package_name', type=str, required=True)
-out_parser.add_argument('--cur_version', type=str, required=False)
-out_parser.add_argument('--available_versions', nargs='+', type=str, required=False)
-out_parser.add_argument('--verbose', type=int, required=False, default=1)
-args = out_parser.parse_args().__dict__
 
-# try to read cur_version
-if args["cur_version"] is None:
-    ex = subprocess.Popen(f'pip list | grep "{args["package_name"]} "', shell=True, stdout=subprocess.PIPE)
-    out, _ = ex.communicate()
-    out = out.decode().strip()
-    # breakpoint()
-    args["cur_version"] = out.split(args["package_name"])[-1].strip()
+def check_version_and_update(package_name, cur_version=None, available_versions=None):
+    """
+        检查当前版本，并尝试更新
+            - 若在 pip 的可用版本中，有比当前版本更高的版本，则更新到可以获取到的最新版本。
+    """
+    # try to read cur_version
+    if cur_version is None:
+        ex = subprocess.Popen(f'pip list | grep "{package_name} "', shell=True, stdout=subprocess.PIPE)
+        out, _ = ex.communicate()
+        out = out.decode().strip()
+        # breakpoint()
+        cur_version = out.split(package_name)[-1].strip()
 
-# try to read available versions
-if args["available_versions"] is None:
-    ex = subprocess.Popen(f'pip install {args["package_name"]}==?', shell=True, stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT)
-    out, _ = ex.communicate()
-    out = out.decode().strip()
-    if "(from versions:" in out:
-        v_ls = out.split("(from versions:")[-1].rsplit(")", 1)[0].split(",", -1)
-        v_ls = [i.strip() for i in v_ls]
+    # try to read available versions
+    if available_versions is None:
+        ex = subprocess.Popen(f'pip install {package_name}==?', shell=True, stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT)
+        out, _ = ex.communicate()
+        out = out.decode().strip()
+        if "(from versions:" in out:
+            v_ls = out.split("(from versions:")[-1].rsplit(")", 1)[0].split(",", -1)
+            v_ls = [i.strip() for i in v_ls]
+        else:
+            v_ls = ["none"]
+        available_versions = version.sort_ls(version_ls=v_ls, reverse=True)
+
+    b_success_updated = False
+    new_version = None
+    if len(available_versions) > 0 and version.compare(available_versions[0], ">", cur_version):
+        ex = subprocess.Popen(
+            f'pip install {package_name}=={available_versions[0]} --no-dependencies',
+            shell=True, stdout=subprocess.PIPE
+        )
+        out, _ = ex.communicate()
+        msg = out.decode().strip()
+        if ex.returncode == 0:
+            b_success_updated = True
+            new_version = available_versions[0]
     else:
-        v_ls = ["none"]
-    args["available_versions"] = version.sort_ls(version_ls=v_ls, reverse=True)
+        msg = "Already the latest version, no need to update"
 
-if len(args["available_versions"]) > 0 and version.compare(args["available_versions"][0], ">", args["cur_version"]):
-    ex = subprocess.Popen(
-        f'pip install {args["package_name"]}=={args["available_versions"][0]} --no-dependencies',
-        shell=True, stdout=subprocess.PIPE
-    )
-    out, _ = ex.communicate()
-    res = out.decode().strip()
-else:
-    res = "Already the latest version, no need to update"
+    res_s = dict(version_before_updated=cur_version, version_after_updated=new_version,
+                 available_versions=available_versions, b_success_updated=b_success_updated, msg=msg)
 
-if args["verbose"]:
-    # print(args)
-    print(res)
+    return res_s
+
+
+if __name__ == '__main__':
+    import argparse
+
+    out_parser = argparse.ArgumentParser(description='check_version_and_update')
+    out_parser.add_argument('--package_name', type=str, required=True)
+    out_parser.add_argument('--cur_version', type=str, required=False)
+    out_parser.add_argument('--available_versions', nargs='+', type=str, required=False)
+    out_parser.add_argument('--verbose', type=lambda x: bool(eval(x)), required=False, default=True)
+    args = out_parser.parse_args().__dict__
+
+    b_version = args.pop("verbose")
+
+    res_s_ = check_version_and_update(**args)
+
+    if b_version:
+        for k, v in res_s_.items():
+            print(f"{k}: {v}")
